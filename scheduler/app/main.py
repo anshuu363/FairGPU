cc19ca371a6d1c85299ec99cfbaa968655df010c
Anshuman Joshi <joshianshuman0@gmail.com>
2026-08-12 19:20:33 +0530

    added schemas.py to take input from user

--- scheduler/app/main.py
+++ scheduler/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from .config import settings
-from .database import engine, Base
-from . import models
+from .database import engine, Base,get_db
+from . import models,schemas
+from sqlalchemy.orm import Session
 
 Base.metadata.create_all(bind=engine)
 
@@ -17,3 +18,16 @@ async def health():
     return {"status": "healthy",
             "heartbeat interval":settings.heartbeat_interval
             }
+
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(
+        name=user.name,
+        email=user.email
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
