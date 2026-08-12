85955f5453229a2f690a3d2f5fbe617843fbe45e
Anshuman Joshi <joshianshuman0@gmail.com>
2026-08-12 21:48:08 +0530

    Added get users and get users by id

--- scheduler/app/main.py
+++ scheduler/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,HTTPException
 from .config import settings
 from .database import engine, Base,get_db
 from . import models,schemas
@@ -31,3 +31,17 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
 
     return db_user
+@app.get("/users", response_model=list[schemas.UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
